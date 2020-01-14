1b941c9b7da6a423c0d4b0a71b77c2e8643178c8
Theodor Jurgens <theodor.jurgens99@hotmail.fr>
2020-01-14 21:39:20 +0100

    seeds baby

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+  #  @user = User.new(user_params)
+
+ #   private
+
+   # def user_params
+    #    params.require(:user).permit(:name, :gender, :age, :email, :photo)
+     # end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#create_table "users", force: :cascade do |t|
+puts 'starts seeding'
+User.destroy_all
+
+user1 = User.create!(name:'Pierre', gender: 'Male', age: 31, email: 'pierre@seeds.com', password: 'password', photo: 'https://cdn.getyourguide.com/img/tour_img-1261167-146.jpg')
+#user2 << [name:'Stéphanie',gender: 'Female',age: 42,email: 'stef@seeds.com']
+#user3 <<[name: 'Patricia',gender: 'Female',age: 22,email: 'pat@seeds;com']
+
+puts "Now we have #{User.count}"
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'finish seeding'

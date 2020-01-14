10a645e325734562b0f0020613093b3b09e993d2
Yanan Dherissart <185847679@qq.com>
2020-01-14 21:34:35 +0100

    user model crud done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
-  def home
+  def index
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    User.find(params[:id]).update(user_params)
+    redirect_to "/users/#{params[:id]}"
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end

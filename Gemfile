b2348ad518d3aa762d7ed79ad7d4d927b72ce458
Yanan Dherissart <185847679@qq.com>
2020-01-16 21:20:17 +0100

    experiences and bookings CRUD done

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'devise'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
+gem 'simple-form-datepicker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'experiences/index'
-  get 'experiences/new'
-  get 'experiences/show'
+  get 'bookings/index'
+  get 'bookings/create'
+  get 'bookings/new'
+  get 'bookings/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#home"
-
   resources :users
 
+  resources :experiences do
+    resources :bookings, only: [:new, :create, :index]
+  end
+
+  resources :bookings, only: [:destroy]
 end

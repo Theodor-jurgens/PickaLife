e9ece2905da4e49cf57a257f948d3ab4c1c7780e
Julien Bisagni <julienbisagni@gmail.com>
2020-01-24 19:02:13 +0100

    Added Photo to User and created the userCard component.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :bookings
   has_many :experiences
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,42 +3,65 @@ require "open-uri"
 Experience.destroy_all
 User.destroy_all
 
-user = User.new(
-  name: "Julien Bisagni",
+user1 = User.new(
+  name: "Jacky Jones",
   gender: "Male",
-  age: 25,
-  email: "julienbisagni@gmail.com",
+  age: 26,
+  email: "jacky@gmail.com",
   password: "121212"
 )
-user.save!
-
+jacky = File.open("app/assets/images/users/jacky.png")
+user1.photo.attach(io: jacky, filename: 'nes.png', content_type: 'image/png')
+user1.save!
 
 experience1 = Experience.new(
   activity: "Surfing",
   place: "Rio",
-  description: "I will be surfing in Rio de Janeiro next week",
+  description: "I will be surfing in Rio de Janeiro next week, come take the wave with me!",
   start_date: 2019-01-20,
   end_date: 2019-01-20,
   price: 20,
-  user: User.first
+  user: User.last
 )
 
 file1 = File.open("app/assets/images/backgrounds/surfing.jpeg")
 experience1.photo.attach(io: file1, filename: 'nes.png', content_type: 'image/png')
 experience1.save!
 
+user2 = User.new(
+  name: "Pierre Jean",
+  gender: "Male",
+  age: 55,
+  email: "pj@gmail.com",
+  password: "121212"
+)
+pj = File.open("app/assets/images/users/pierre-jean.jpeg")
+user2.photo.attach(io: pj, filename: 'nes.png', content_type: 'image/png')
+user2.save!
+
 experience2 = Experience.new(
   activity: "Smoking",
   place: "Paris",
-  description: "What's better than a fine cigarette in Paris?",
+  description: "I will be smoking my last cigarette in the Parisian streets. Enjoy that one with me !",
   start_date: 2019-01-20,
   end_date: 2019-01-20,
   price: 5,
-  user: User.first
+  user: User.last
 )
 file2 = File.open("app/assets/images/backgrounds/smoking.jpeg")
 experience2.photo.attach(io: file2, filename: 'nes.png', content_type: 'image/png')
 experience2.save!
+
+user3 = User.new(
+  name: "Alvaro Vega",
+  gender: "No gender",
+  age: 40,
+  email: "alva@gmail.com",
+  password: "121212"
+)
+alvaro = File.open("app/assets/images/users/alvaro.jpeg")
+user3.photo.attach(io: alvaro, filename: 'nes.png', content_type: 'image/png')
+user3.save!
 
 experience3 = Experience.new(
   activity: "Running",
@@ -47,11 +70,22 @@ experience3 = Experience.new(
   start_date: 2019-01-20,
   end_date: 2019-01-20,
   price: 10,
-  user: User.first
+  user: User.last
 )
 file3 = File.open("app/assets/images/backgrounds/running.jpeg")
 experience3.photo.attach(io: file3, filename: 'nes.png', content_type: 'image/png')
 experience3.save!
+
+user4 = User.new(
+  name: "Yanan Dh",
+  gender: "Female",
+  age: 26,
+  email: "yaya@gmail.com",
+  password: "121212"
+)
+yanan = File.open("app/assets/images/users/yanan.jpeg")
+user4.photo.attach(io: yanan, filename: 'nes.png', content_type: 'image/png')
+user4.save!
 
 experience4 = Experience.new(
   activity: "Shopping",
@@ -60,7 +94,7 @@ experience4 = Experience.new(
   start_date: 2019-01-20,
   end_date: 2019-01-20,
   price: 50,
-  user: User.first
+  user: User.last
 )
 file4 = File.open("app/assets/images/backgrounds/shopping.jpeg")
 experience4.photo.attach(io: file4, filename: 'nes.png', content_type: 'image/png')

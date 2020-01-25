3e34528ebd80c865c6130640c13db543d013ee91
Yanan Dherissart <185847679@qq.com>
2020-01-25 11:35:54 +0100

    migration review model done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_214043) do
+ActiveRecord::Schema.define(version: 2020_01_25_100900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,11 +60,13 @@ ActiveRecord::Schema.define(version: 2020_01_24_214043) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.text "comment"
+    t.string "comment"
+    t.bigint "user_id"
+    t.bigint "experience_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "experience_id"
+    t.index ["experience_id"], name: "index_reviews_on_experience_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,4 +88,6 @@ ActiveRecord::Schema.define(version: 2020_01_24_214043) do
   add_foreign_key "bookings", "experiences"
   add_foreign_key "bookings", "users"
   add_foreign_key "experiences", "users"
+  add_foreign_key "reviews", "experiences"
+  add_foreign_key "reviews", "users"
 end
